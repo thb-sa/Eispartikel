b0e42fbdfbe25ec93613f183871f014443443ba7
kaulima19 <kaulima19@gmail.com>
2016-11-25 22:07:51 +0100

    Mehr Kommentierung der Beispielclients & Nutzung der neusten Kommunikationsbibliothek

--- src/main/java/Softwarearchitektur/EispartikelTestClient/TestAenderung.java
+++ src/main/java/Softwarearchitektur/EispartikelTestClient/TestAenderung.java
@@ -3,27 +3,56 @@ package Softwarearchitektur.EispartikelTestClient;
 import java.io.IOException;
 
 import datenKlassen.Aenderungsmeldung;
-import datenKlassen.NeuesObjektListener;
 import datenKlassen.StationAenderung;
-import datenKlassen.ZweiwegeClientkommunikator;
+import funktionaleKlassen.NeuesObjektListener;
+import funktionaleKlassen.ZweiwegeClientkommunikator;
 
+/**
+ * Diese Klasse dient als Beispiel zur Implementierung der Verbindung zum Server
+ * auf der alle aktuellen Werte versendet werden.
+ * 
+ * @author Mario Kaulmann
+ * 
+ */
 public class TestAenderung {
 
 	public static void main(String args[]) {
 		try {
+			/*
+			 * Hier muss der ZweiwegeClientkommunikator angewendet werden. Der
+			 * Konstruktor hat als Parameter die IP des Servers, die
+			 * Implementierung des Listeners fuer empfangene Objekte und die
+			 * Portnummer ueber die gearbeitet wird. Die Portnummer ist in der
+			 * Kommunikatorklasse bereits als Konstante hinterlegt. In den
+			 * spitzen Klammern die Typen sind zuerst die Empfangenen und dann
+			 * die zu versendenden Typen.
+			 */
 			ZweiwegeClientkommunikator<Aenderungsmeldung, StationAenderung> zCK = new ZweiwegeClientkommunikator<Aenderungsmeldung, StationAenderung>(
 					"127.0.0.1", new NeuesObjektListener<Aenderungsmeldung>() {
-
+						/*
+						 * Anonyme Implementierung des Interfaces: kein guter
+						 * Stil, aber nur ein Beispiel
+						 */
 						public void neuesAustauschobjekt(
 								Aenderungsmeldung austauschobjekt) {
 							System.out.println(austauschobjekt.getAbweichung());
 						}
 					}, ZweiwegeClientkommunikator.ZWEIWEGEKOMMUNIKATION);
 
+			/*
+			 * Sehr wichtig: das Starten des Threads nicht vergessen
+			 */
 			zCK.start();
 
+			/*
+			 * Hier wird jetzt beispielsweise eine StationsAenderung versendet.
+			 */
 			zCK.versende(new StationAenderung(args[0], args[1], 50));
 
+			/*
+			 * Der Client lauft weiter, da er immernoch mithoert, was es fuer
+			 * Aenderungen gibt.
+			 */
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
---
+++ src/main/java/Softwarearchitektur/EispartikelTestClient/TestAnmeldung.java
@@ -0,0 +1,85 @@
+package Softwarearchitektur.EispartikelTestClient;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import datenKlassen.Station;
+import funktionaleKlassen.EinwegClientkommunikator;
+import funktionaleKlassen.NeuesObjektListener;
+
+/**
+ * Diese Klasse dient als Beispiel dafuer, wie eine Verbindung zum Server
+ * hergestellt werden kann. Hier geht es um die Verbindung zum Server um die
+ * Stationen zu beziehen.
+ * 
+ * @author Mario Kaulmann
+ * 
+ */
+public class TestAnmeldung {
+
+	public static void main(String args[]) {
+		try {
+			/*
+			 * Implementierung der Inneren Klasse siehe unten.
+			 */
+			TestAnmeldung.KomTestimplements listener = new KomTestimplements();
+
+			/*
+			 * Benutzung des Einwegclients. Uebergabewerte: IP des Servers,
+			 * Objekt, das den Listener implementiert und der Port, der in der
+			 * Klasse allerdings als Konstante hinterlegt ist.
+			 */
+			EinwegClientkommunikator<Station> ew = new EinwegClientkommunikator<Station>(
+					"127.0.0.1", listener,
+					EinwegClientkommunikator.EINWEGKOMMUNIKATION);
+			/*
+			 * Sehr wichtig: den Thread starten.
+			 */
+			ew.start();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Diese innere Klasse dient nur dazu das Interface zu implementieren und
+	 * ist kein Beispiel fuer guten Stil. Der Typ, der in den spitzen Klammern
+	 * eingetragen wird ist der Typ, den ich als zu empfangenes Objekt erwarte.
+	 * 
+	 * @author Mario Kaulmann
+	 * 
+	 */
+	private static class KomTestimplements implements
+			NeuesObjektListener<Station> {
+		/*
+		 * Implementierung der Methode aus dem Interface. Im Hintergrund lauft
+		 * der Thread, der immer neue stationen empfaengt. Deshalb ist es
+		 * notwendig die Implementierung so vorzunehmen, dass keine
+		 * Zugriffskonflikte entstehen koennen.
+		 */
+		public void neuesAustauschobjekt(Station station) {
+			/*
+			 * Die Station, die uns neu uebergeben wurde wird hier nur
+			 * ausgegeben.
+			 */
+			System.out.println(station.getStationID() + " "
+					+ station.getVorgabewert());
+			/*
+			 * Hier werden falls vorhanden die Aktuellen Tageswerte ausgegeben.
+			 */
+			for (String key : station.getAktuelleWerte().keySet()) {
+				System.out.println("\t"
+						+ key
+						+ " "
+						+ station.getAktuelleWerte().get(key)
+								.getAktuellerWert() + " / "
+						+ station.getAktuelleWerte().get(key).getAbweichung());
+			}
+		}
+	}
+}
